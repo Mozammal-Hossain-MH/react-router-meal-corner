e7ac7ef0070639adf5ef708e32311c25858ba95f
mozammal hossain <mozammalhossain33@gmail.com>
2024-01-21 12:03:14 +0600

    some issue fixed

--- src/Components/CategoryDetails/CategoryDetails.jsx
+++ src/Components/CategoryDetails/CategoryDetails.jsx
@@ -15,9 +15,9 @@ const CategoryDetails = () => {
 
     return (
         <div>
-            <div className='flex justify-between items-center my-20 mx-5'>
+            <div className='flex justify-between items-center my-10 sm:my-20 ml-3 sm:mx-5 '>
                 <p className='font-bold sm:text-lg lg:text-5xl'>Choose Your Special Meal Today!</p>
-                <button onClick={handleGoBack} className='bg-yellow-500 m-5 py-1 px-10 rounded-md border-b-4 border-solid border-yellow-800 hover:bg-yellow-400 text-white'>Go Back</button>
+                <button onClick={handleGoBack} className='bg-yellow-500 m-5 py-1 px-3 md:px-10 rounded-md border-b-4 border-solid border-yellow-800 hover:bg-yellow-400 text-white'>Go Back</button>
             </div>
 
             <div className='grid sm:grid-cols-2 lg:grid-cols-3'>
